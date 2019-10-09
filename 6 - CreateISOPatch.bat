f2f15910f7a3650b34eba772198acec9f6110e3f
colebob9 <colebob9@gmail.com>
2019-10-09 15:58:44 -0500

    Create zip archive when creating patch file

--- 6 - CreateISOPatch.bat
+++ 6 - CreateISOPatch.bat
@@ -10,4 +10,13 @@ copy ..\executables\xdelta3.exe
 
 copy ..\executables\PatchISO.bat
 
+:: automatically create zip archive for patch distribution
+cd ..
+
+set zipName=NichiEngPatch_%date:~10,4%-%date:~4,2%-%date:~7,2%
+
+"C:\Program Files\7-Zip\7z.exe" a %zipName%.zip .\patch_distrib\*
+
+
+
 PAUSE
