056dc3d6d4acd7e9bcfa8a0b6834a0baf055c441
colebob9 <colebob9@gmail.com>
2019-10-14 18:25:12 -0500

    Added script for cleaning up previous patch processes

---
+++ Cleanup.bat
@@ -0,0 +1,20 @@
+:: Cleanup script
+@ECHO OFF
+
+:: Wait first
+echo This will clean up anything the scripts have generated.
+echo Please exit if you do not want files to be deleted.
+echo.
+echo The script will delete the following folders (only in this directory):
+echo temp_pack, temp_patch, cpkdumps, dump, patch_distrib
+echo.
+PAUSE
+
+:: Delete generated folders
+RD /S /Q temp_pack
+RD /S /Q temp_patch
+RD /S /Q cpkdumps
+RD /S /Q dump
+RD /S /Q patch_distrib
+
+PAUSE
